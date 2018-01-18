f45bc9be840df0c6786a8da26d81ebb997a4ed15
Nico <nico1099@hotmail.fr>
2018-01-18 09:10:04 +0100

    Nettoyage du code pour meilleure lisibilité :)

--- nicobot_access.rb
+++ nicobot_access.rb
@@ -10,59 +10,45 @@ $client = Twitter::REST::Client.new do |config|
       config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
 end
 
+$clientstream = Twitter::Streaming::Client.new do |config|
+  config.consumer_key        = ENV["API_KEY"]
+  config.consumer_secret     = ENV["API_SECRET"]
+  config.access_token        = ENV["ACCESS_TOKEN"]
+  config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
+end
 
 #Pour publier un tweet "Hello"
-# $client.update('Hello')
-
-
-clientstream = Twitter::Streaming::Client.new do |config|
-  config.consumer_key        = ENV["API_KEY"]
-  config.consumer_secret     = ENV["API_SECRET"]
-  config.access_token        = ENV["ACCESS_TOKEN"]
-  config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
+def message(texte)
+  $client.update(texte)
 end
+#message("Hello!")
 
-# streamer tous les tweets qui mentionnent tea, ou coffee. == Fonctionnel
-=begin
-topics = ["coffee", "tea"]
-clientstream.filter(track: topics.join(",")) do |object|
-  puts object.text if object.is_a?(Twitter::Tweet)
+
+#Pour envoyer un message à plusieurs personnes avec personnalisation
+def message_commun(people)
+  people.each do |user|
+    message = 'Hello ! ' + user
+    $client.update(message)
+    end
 end
-=end
+# message_commun(["@ThpTest","@Fabien_971"])
 
 
-#Pour follow une liste de twittos  == Fonctionnel
-=begin
+
+# streamer tous les tweets qui mentionnent tea, ou coffee.
+def topic_stream(*topics)
+  $client.filter(track: topics.join(",")) { |object|
+    puts object.text if object.is_a?(Twitter::Tweet)  #On filtre tous les éléments trouvés de type tweet
+  }
+end
+#topic_stream(["coffee","tea"])
+
+
+#Pour follow une liste de twittos
 def to_follow(*liste_personnes)
-  liste_personnes.each{ |twittos| $client.follow(twittos) }
+  liste_personnes.each{ |twittos|
+    $client.follow(twittos)
+    print "Follow #{twittos} : C'est fait !"
+  }
 end
 to_follow("Fabien_971","Mamarieponpont")
-=end
-
-#print $client.user("Mamarieponpont")
-
-
-
-
-
-
-
-=begin CHANTIER
-$client.update_with_media("Coding with #THP!", File.new(url="https://media.giphy.com/media/ULR7kNP2lhJXW/giphy.gif"))
-
-#Envoyer un Direct Message ou message privé à un abonné
-
-
-
-
-clientmess = Twitter::DirectMessage::Client.new do |config|
-  config.consumer_key        = ENV["API_KEY"]
-  config.consumer_secret     = ENV["API_SECRET"]
-  config.access_token        = ENV["ACCESS_TOKEN"]
-  config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
-end
-
-clientmess(destinataire)
-client.update('Hello')
-
-=end
